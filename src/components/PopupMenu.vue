40a05a643efcd33eb1866b285bf6e3d5befff99b
anand kumar <2anandkr@gmail.com>
2017-11-17 10:51:43 +0530

    Feature/popup (#10)
    * feat: add a generic popup widget & create a popup menu with it.
    
    * feat: in popup menu optionally keep track of item clicks.
    
    Use case for tracking clicked items: in filters you might want to track each filter item.

---
+++ src/components/PopupMenu.vue
@@ -0,0 +1,89 @@
+<template>
+  <Popup :left="left" :top="top" :cue="cue">
+    <ul class="popup-menu">
+      <li v-for="(item, index) in items" :key="index">
+        <a :href="item.link" 
+           class="popup-menu__link"
+           @click="(trackSelected ? selectItem(index, $event): null)">
+          <template v-if="item.icon">
+            <i 
+              class="popup-menu__icon fa" 
+              :class="'fa-' + item.icon"
+              aria-hidden="true">
+              &nbsp;
+            </i>
+          </template>
+          <template v-if="item.image">
+            <img class="popup-menu__image" :src="item.image">&nbsp;
+          </template>
+          <span class="popup-menu__label">{{item.label}}</span>
+        </a>
+      </li>
+    </ul>
+
+  </Popup>
+
+</template>
+
+<script>
+import Popup from '@/components/Popup';
+
+export default {
+  components: { Popup },
+  props: ['trackSelected', 'items', 'left', 'top', 'cue'],
+  methods: {
+    selectItem(index, event) {
+      event.preventDefault();
+      this.$emit('selected', index);
+    },
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+.popup-menu {
+    list-style: none;
+    margin: 0;
+    min-width: 10em;
+    background-color: #fff;
+    font-size: 1rem;
+
+  &--small {
+    font-size: 1rem;
+  }
+
+  &--big {
+    font-size: 1.6rem;
+  }
+
+  &__link {
+    display: block;
+    color: #555;
+    font-family: 'Arial';
+    padding: 0.5em 0.7em;
+    text-decoration: none;
+  }
+
+  &__link:hover {
+      background-color: #f3f3f3;
+      color: #777;
+  }
+
+  &__image {
+    width: 1.2em;
+    height: 1.2em;
+    border-radius: 50%;
+    vertical-align: middle;
+  }
+
+  &__icon {
+    vertical-align: middle;
+    font-size: 1.2em;
+  }
+
+  &__label {
+    font-size: 0.9em;
+  }
+}
+</style>
